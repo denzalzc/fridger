3eaac2b0397ef454d0586633cca354f63c2363c0
denzalzc <deniszabolzc@gmail.com>
2026-02-01 20:40:23 +0400

    adds logger

--- src/fridger/logger.py
+++ src/fridger/logger.py
@@ -9,6 +9,12 @@ class FridgeLogger:
     def __init__(self, filename):
         self.filename = filename
 
+        if not(f"{LOGS_DIR}/{self.filename}" in os.listdir(LOGS_DIR)):
+            with open(f"{LOGS_DIR}/{self.filename}", 'w') as file:
+                file.close()
+
+    
+
     def info(self, text):
         with open(f"{LOGS_DIR}/{self.filename}", 'a') as file:
             file.write(f"[INFO] [{datetime.now().strftime("%d/%m/%Y, %H:%M:%S")}]: {text}\n")
